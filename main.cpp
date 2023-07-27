a12499f60cd18f0c90c8a6d9a51a37a8733e0e71
zhongshuheng <58834725+shuheng-2834@users.noreply.github.com>
2023-07-27 10:44:13 +0800

    第一次漫反射得到的球体

--- main.cpp
+++ main.cpp
@@ -6,49 +6,63 @@
 
 #include <iostream>
 
-vec3 ray_color(const ray &r, const hittable &world)
+vec3 ray_color(const ray &r, const hittable &world, int depth)
 {
     hit_record rec;
+
+    if (depth < 0)
+    {
+        return color(0, 0, 0);
+    }
+
     if (world.hit(r, 0, infinity, rec))
     {
-        return 0.5 * (rec.normal + color(1, 1, 1));
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
     }
     vec3 unity_direction = unit_vector(r.direction());
     auto t = 0.5 * (unity_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-int main() {
+int main()
+{
 
     // Image
 
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 1000;
+    const int samples_per_pixel = 100;
+
+    const int max_depth = 50;
 
     // World
 
     hittable_list world;
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
     camera cam;
 
     // Render
 
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+    std::cout << "P3\n"
+              << image_width << " " << image_height << "\n255\n";
 
-    for (int j = image_height-1; j >= 0; --j) {
+    for (int j = image_height - 1; j >= 0; --j)
+    {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
+        for (int i = 0; i < image_width; ++i)
+        {
             color pixel_color(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j + random_double()) / (image_height-1);
+            for (int s = 0; s < samples_per_pixel; ++s)
+            {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
--- vec3.h
+++ vec3.h
@@ -6,107 +6,142 @@
 
 using std::sqrt;
 
-class vec3 {
-    public:
-        vec3() : e{0,0,0} {}
-        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+class vec3
+{
+public:
+    vec3() : e{0, 0, 0} {}
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-        double x() const { return e[0]; }
-        double y() const { return e[1]; }
-        double z() const { return e[2]; }
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
 
-        vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-        double operator[](int i) const { return e[i]; }
-        double& operator[](int i) { return e[i]; }
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    double operator[](int i) const { return e[i]; }
+    double &operator[](int i) { return e[i]; }
 
-        vec3& operator+=(const vec3 &v) {
-            e[0] += v.e[0];
-            e[1] += v.e[1];
-            e[2] += v.e[2];
-            return *this;
-        }
+    vec3 &operator+=(const vec3 &v)
+    {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
 
-        vec3& operator*=(const double t) {
-            e[0] *= t;
-            e[1] *= t;
-            e[2] *= t;
-            return *this;
-        }
+    vec3 &operator*=(const double t)
+    {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
 
-        vec3& operator/=(const double t) {
-            return *this *= 1/t;
-        }
+    vec3 &operator/=(const double t)
+    {
+        return *this *= 1 / t;
+    }
 
-        double length() const {
-            return sqrt(length_squared());
-        }
+    double length() const
+    {
+        return sqrt(length_squared());
+    }
 
-        double length_squared() const {
-            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
-        }
-        
+    double length_squared() const
+    {
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
 
-    public:
-        double e[3];
+public:
+    double e[3];
 };
 
 // Type aliases for vec3
-using point3 = vec3;   // 3D point
-using color = vec3;    // RGB color
+using point3 = vec3; // 3D point
+using color = vec3;  // RGB color
 
 // vec3 Utility Functions
 
 // 运算符重载，<< 用于输出，inline 用于内联函数
-inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+inline std::ostream &operator<<(std::ostream &out, const vec3 &v)
+{
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
 // 加法运算符重载
-inline vec3 operator+(const vec3 &u, const vec3 &v) {
+inline vec3 operator+(const vec3 &u, const vec3 &v)
+{
     return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
 // 减法运算符重载
-inline vec3 operator-(const vec3 &u, const vec3 &v) {
+inline vec3 operator-(const vec3 &u, const vec3 &v)
+{
     return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
 // 乘法运算符重载
-inline vec3 operator*(const vec3 &u, const vec3 &v) {
+inline vec3 operator*(const vec3 &u, const vec3 &v)
+{
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
 // 乘法运算符重载
-inline vec3 operator*(double t, const vec3 &v) {
-    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+inline vec3 operator*(double t, const vec3 &v)
+{
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
 // 乘法运算符重载
-inline vec3 operator*(const vec3 &v, double t) {
+inline vec3 operator*(const vec3 &v, double t)
+{
     return t * v;
 }
 
 // 除法运算符重载
-inline vec3 operator/(vec3 v, double t) {
-    return (1/t) * v;
+inline vec3 operator/(vec3 v, double t)
+{
+    return (1 / t) * v;
 }
 
 // 返回点乘结果
-inline double dot(const vec3 &u, const vec3 &v) {
-    return u.e[0] * v.e[0]
-         + u.e[1] * v.e[1]
-         + u.e[2] * v.e[2];
+inline double dot(const vec3 &u, const vec3 &v)
+{
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
 }
 
 // 返回叉乘结果
-inline vec3 cross(const vec3 &u, const vec3 &v) {
+inline vec3 cross(const vec3 &u, const vec3 &v)
+{
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
                 u.e[2] * v.e[0] - u.e[0] * v.e[2],
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
 // 返回单位向量
-inline vec3 unit_vector(vec3 v) {
+inline vec3 unit_vector(vec3 v)
+{
     return v / v.length();
+}
+// 随机数生成
+inline vec3 random()
+{
+    return vec3(random_double(), random_double(), random_double());
+}
+
+// 随机生成指定范围的数
+inline vec3 random(double min, double max)
+{
+    return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+}
+
+vec3 random_in_unit_sphere()
+{
+    while (true)
+    {
+        auto p = random(-1, 1);
+        if (p.length_squared() >= 1)
+            continue;
+        return p;
+    }
 }
 #endif
