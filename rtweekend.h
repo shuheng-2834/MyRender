58e023c23430c0ef36582cafc19251a5657e7019
zhongshuheng <58834725+shuheng-2834@users.noreply.github.com>
2023-07-27 09:30:04 +0800

    加入图片采样，通过对像素的多重采样来减少锯齿

--- rtweekend.h
+++ rtweekend.h
@@ -23,6 +23,28 @@ inline double degrees_to_radians(double degrees)
 inline double ffmin(double a, double b) { return a <= b ? a : b; }
 inline double ffmax(double a, double b) { return a >= b ? a : b; }
 
+inline double random_double()
+{
+    // 返回[0,1)之间的随机数
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+    // 返回[min,max)之间的随机数
+    return min + (max - min) * random_double();
+}
+/// @brief 限制x的范围在[min,max]之间
+/// @param x 输入的值
+/// @param min 最小值
+/// @param max 最大值
+/// @return 返回限制后的值
+inline double clamp(double x,double min,double max){
+    if(x < min) return min;
+    if(x > max) return max;
+    return x;
+}
+
 // Common Headers
 #include "ray.h"
 #include "vec3.h"
